0c636dc19f3d4fa97bb91b114f8a41d7eb50d409
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-04-26 07:44:32 -0700

    Add files via upload

---
+++ MileageGas.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class MileageGas{
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	int total = 0;
+	int number = 0;
+	int division = 0;
+
+	while(number != -1) {
+
+	System.out.print("THe miles driven: ");
+	int number1 = input.nextInt();
+
+	System.out.print("The gallon used: ");
+	int number2 = input.nextInt();
+
+	division = number1 / number2;	
+	total = total + division;
+
+	System.out.println("The miles per gallon: " + division);
+	
+	System.out.print("Enter a number to keep on going OR -1 to quit: ");
+	number = input.nextInt();
+}
+	
+	System.out.println("The total gallon used: " + total);
+	
+
+
+
+
+
+}
+
+
+
+}
