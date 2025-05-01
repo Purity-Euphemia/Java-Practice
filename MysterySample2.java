537cc09d3a377bdc19dd25272e2cd797af94f03c
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-04-30 20:44:35 -0700

    Pratices

---
+++ MysterySample2.java
@@ -0,0 +1,13 @@
+public class MysterySample2 {
+	public static void main(String[] args) {
+
+	int count = 1;
+	while (count <= 20) {
+	System.out.println(count % 3 == 1 ? "########" : "++++++++");
+	++count;
+
+
+
+}
+}
+}
